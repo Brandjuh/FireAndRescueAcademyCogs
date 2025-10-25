a083da61644a47a8d30a4c0a3569cc35bdaa6545
Brandjuh <Brandjuh@users.noreply.github.com>
2025-10-25 14:07:50 +0200

    Update __init__.py

--- assetmanager/__init__.py
+++ assetmanager/__init__.py
@@ -1,6 +1,3 @@
 from .assetmanager import AssetManager, setup
 
 __red_end_user_data_statement__ = "This cog does not store any end user data."
-# Utils package init
-from .embeds import *
-from .views import *
