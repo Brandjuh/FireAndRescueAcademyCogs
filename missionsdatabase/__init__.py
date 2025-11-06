d5413ebc888d4ea0aae218f85f3816d7918c7eea
Brandjuh <Brandjuh@users.noreply.github.com>
2025-11-07 00:17:29 +0100

    Simplify cog addition in setup function

--- missionsdatabase/__init__.py
+++ missionsdatabase/__init__.py
@@ -3,5 +3,4 @@ from .missions_database import MissionsDatabase
 
 async def setup(bot):
     """Load MissionsDatabase cog."""
-    cog = MissionsDatabase(bot)
-    await bot.add_cog(cog)
+    await bot.add_cog(MissionsDatabase(bot))
