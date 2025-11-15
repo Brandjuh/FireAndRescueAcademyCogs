167002950e1033c8a86cb2023a4676e06712035d
Brandjuh <Brandjuh@users.noreply.github.com>
2025-11-15 20:26:05 +0100

    Update __init__.py

--- icongen/__init__.py
+++ icongen/__init__.py
@@ -1,1 +1,10 @@
-icongen/
+"""
+IconGen cog initialization
+"""
+
+from .icongen import IconGen
+
+async def setup(bot):
+    """Load IconGen cog"""
+    cog = IconGen(bot)
+    await bot.add_cog(cog)
